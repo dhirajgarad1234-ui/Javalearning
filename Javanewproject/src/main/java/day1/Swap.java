94b689809cf9866c417de0ce8aa64674e1d5dec8
dhirajgarad1234-ui <dhirajgarad1234@gmail.com>
2025-09-28 13:44:16 +0530

    commit

---
+++ Javanewproject/src/main/java/day1/Swap.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class Swap {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        a = a + b;  // a = 15
+        b = a - b;  // b = 5
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
