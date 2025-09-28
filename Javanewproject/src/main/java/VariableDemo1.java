44ccb2d0486dcdb1219672a196015db56b300c0a
dhirajgarad1234-ui <dhirajgarad1234@gmail.com>
2025-09-28 18:59:19 +0530

    commit

---
+++ Javanewproject/src/main/java/VariableDemo1.java
@@ -0,0 +1,12 @@
+public class VariableDemo1 {
+    public static void main(String[] args) {
+        //Substraction
+
+        float a =10.5f;
+        float b =5.4f;
+
+        a=a*b;
+
+        System.out.println("a=" +a);
+    }
+}
