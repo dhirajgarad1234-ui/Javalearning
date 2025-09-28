a468c5aac19a7586a2b0f3465cfd31048312cc70
dhirajgarad1234-ui <dhirajgarad1234@gmail.com>
2025-09-28 17:19:27 +0530

    Update Swap.java

--- Javanewproject/src/main/java/day1/Swap.java
+++ Javanewproject/src/main/java/day1/Swap.java
@@ -5,12 +5,10 @@ public class Swap {
         int a = 5;
         int b = 10;
 
-        // Swapping
         a = a + b;  // a = 15
         b = a - b;  // b = 5
         a = a - b;  // a = 10
 
-        System.out.println("After swapping:");
         System.out.println("a=" +a);
         System.out.println("b=" +b);
     }
