7c3fdc2dbecb9bb23167a94bdd1fc63de9b69a56
dhirajgarad1234-ui <dhirajgarad1234@gmail.com>
2025-09-29 07:14:04 +0530

    Create VariableDemo2.java

---
+++ Javanewproject/src/main/java/day1/VariableDemo2.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class VariableDemo2 {
+    public static void main(String[] args) {
+        float a =10.5f;
+        float b =5.4f;
+
+        a=a/b;
+        b=a*b;
+
+        System.out.println("a=" +a);
+        System.out.println("b=" +b);
+    }
+
+}
