99281774f760d2ce7fa107f6b5913b9ce38c1d28
dhirajgarad1234-ui <dhirajgarad1234@gmail.com>
2025-10-04 20:30:42 +0530

    Commit

---
+++ Testometer_Sep25/src/main/java/day1/DataTypeDemo.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class DataTypeDemo {
+    public static void main(String[] args) {
+        short S1=171;
+
+        byte B2=(byte) S1;
+
+        System.out.println(B2);
+    }
+}
