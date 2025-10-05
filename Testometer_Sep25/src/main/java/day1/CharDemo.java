94a814901ef7abfca7bda7bbe141e5e7a9c878fd
dhirajgarad1234-ui <dhirajgarad1234@gmail.com>
2025-10-05 16:48:45 +0530

    commit

--- Testometer_Sep25/src/main/java/day1/CharDemo.java
+++ Testometer_Sep25/src/main/java/day1/CharDemo.java
@@ -2,12 +2,18 @@ package day1;
 
 public class CharDemo {
     public static void main(String[] args) {
-       // char c='v';
-      //  int k=c;
-       // System.out.println(k);
+        //Ascii--1byte--Ascii chart
+        char c='v';
+       //System.out.println(c);
+        int k=c;
+        System.out.println(k);
+//all char is integer
+        int n=97; //4byte
+        char d=(char)n;// 1byte
 
-        char d='\u0927';
+       // System.out.println(d);
+      //  char d='\u0927';
        // char h='\u093F';
-        System.out.println("" +d +'\u093F'+'\u0930' +'\u091C');
+       //System.out.println("" +d +'\u093F'+'\u0930' +'\u091C');
     }
 }
