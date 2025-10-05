63da3a3223b600ee637dde5408e9521d15de0929
dhirajgarad1234-ui <dhirajgarad1234@gmail.com>
2025-10-05 17:15:21 +0530

    commit

--- Testometer_Sep25/src/main/java/day1/OperatorDemo.java
+++ Testometer_Sep25/src/main/java/day1/OperatorDemo.java
@@ -2,6 +2,14 @@ package day1;
 
 public class OperatorDemo {
     public static void main(String[] args) {
-
+//Operator +-*/% and Operand numbers eg.10+2 10 and 2 operand
+        //Arithmatic
+        //relational
+        //bitwise
+        //logical
+        //assignment
+        //unary
+        //ternary
+        //shift
     }
 }
---
+++ Testometer_Sep25/src/main/java/day1/SysoutDemo.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class SysoutDemo {
+    public static void main(String[] args) {
+        //System.out.println("Dhiraj" + "Garad");
+        //System.out.println(10+2);
+        //+operator will behave like addition until string not available
+        //As soon as string present + operator change its nature to append
+        System.out.println(10+20+30);
+        System.out.println(""+10+20+30);
+    }
+}
