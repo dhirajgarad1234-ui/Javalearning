bda21a83bc85e8e26240e4f8790c13b08d9c589f
dhirajgarad1234-ui <dhirajgarad1234@gmail.com>
2025-10-05 17:01:10 +0530

    commit

--- Testometer_Sep25/src/main/java/day1/CharDemo.java
+++ Testometer_Sep25/src/main/java/day1/CharDemo.java
@@ -15,6 +15,6 @@ public class CharDemo {
         //unicode;2byte--hexadecimal
        char d='\u0927';
        // char h='\u093F';
-       System.out.println("" +d +'\u093F'+'\u0930' +'\u091C');
+       System.out.println("" +d +'\u093F'+'\u0930' +'\u091C' +'\u0926'+'\u0946'+'\u0935'+'\u093F'+'\u0926'+'\u093E'+'\u0938');
     }
 }
