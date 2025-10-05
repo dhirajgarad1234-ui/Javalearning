2c7c3d65d7981e76686ad4906a2a0c9199b1ab4a
dhirajgarad1234-ui <dhirajgarad1234@gmail.com>
2025-10-05 11:24:21 +0530

    Create CharDemo.java

---
+++ Testometer_Sep25/src/main/java/day1/CharDemo.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class CharDemo {
+    public static void main(String[] args) {
+       // char c='v';
+      //  int k=c;
+       // System.out.println(k);
+
+        char dh='\u0927';
+        char dhi='\u093F';
+        System.out.println("" +dh +dhi +'\u0930' +'\u091C');
+    }
+}
