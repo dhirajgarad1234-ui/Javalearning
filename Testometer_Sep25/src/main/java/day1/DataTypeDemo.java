233286473d1f1db819d80f81242e372ca9c61c9d
dhirajgarad1234-ui <dhirajgarad1234@gmail.com>
2025-10-05 10:41:10 +0530

    Update DataTypeDemo.java

--- Testometer_Sep25/src/main/java/day1/DataTypeDemo.java
+++ Testometer_Sep25/src/main/java/day1/DataTypeDemo.java
@@ -2,10 +2,11 @@ package day1;
 
 public class DataTypeDemo {
     public static void main(String[] args) {
-        short S1=171;
+        short S1=181;
 
         byte B2=(byte) S1;
 
         System.out.println(B2);
+        //type conversion
     }
 }
