a3103ea8ff6f6867e7a4947d37ac4dd8f206857b
dhirajgarad1234-ui <dhirajgarad1234@gmail.com>
2025-10-11 19:37:35 +0530

    commmit

---
+++ Testometer_Sep25/src/main/java/day1/RelationalOperator.java
@@ -0,0 +1,19 @@
+package day1;
+
+public class RelationalOperator {
+    public static void main(String[] args) {
+
+        //comparison --return boolean-- true or false
+        //< > >= <= == !=
+
+        int i = 10;
+        int j = 7;
+        System.out.println(i>j);
+        System.out.println(i<j);
+        System.out.println(i>=j);
+        System.out.println(i<=j);
+        System.out.println(i==j);
+        System.out.println(i!=j);
+    }
+
+}
