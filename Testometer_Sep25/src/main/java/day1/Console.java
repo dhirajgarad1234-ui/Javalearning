8800962bf8af4c5373c5ea35bc15f06d1691b38b
dhirajgarad1234-ui <dhirajgarad1234@gmail.com>
2025-10-11 22:54:20 +0530

    commit

---
+++ Testometer_Sep25/src/main/java/day1/Console.java
@@ -0,0 +1,13 @@
+package day1;
+
+import java.util.Scanner;
+
+public class Console {
+    public static void main(String[] args) {
+        Scanner console=new Scanner(System.in);
+        System.out.println("what is your age");
+        int age= console.nextInt();
+        System.out.println("your age is" +age);
+
+    }
+}
