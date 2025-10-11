9d01af036521f8bdd162b1ed0f2006d6ba857da1
dhirajgarad1234-ui <dhirajgarad1234@gmail.com>
2025-10-11 17:58:56 +0530

    Update ArithmaticOperator.java

--- Testometer_Sep25/src/main/java/day1/ArithmaticOperator.java
+++ Testometer_Sep25/src/main/java/day1/ArithmaticOperator.java
@@ -8,8 +8,23 @@ public class ArithmaticOperator {
         System.out.println(i + j);
         System.out.println(i - j);
         System.out.println(i * j);
-        System.out.println(i / j);
+        System.out.println((float)i / (float)j);
         System.out.println(i % j);
+//Bodmass rule
+        //B → Brackets
+        //O → Orders (powers, roots, etc.)
+        //D → Division
+        //M → Multiplication
+        //A → Addition
+        //S → Subtraction
+       // System.out.println(10/5-3*4);
+//even, odd numbers
+       // int k=23;
+        //k % 2==1 odd number
+        //k % 2==0 even number
+
+        int num=673;
+        System.out.println(num % 10);
         //  int num=67;
         //int sum=0;
         //sum=sum+(num%10);
