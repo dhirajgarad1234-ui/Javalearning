7a74599d1ecaa7653d7eb479af071cb13f36b788
dhirajgarad1234-ui <dhirajgarad1234@gmail.com>
2025-10-11 23:11:47 +0530

    Update Console.java

--- Testometer_Sep25/src/main/java/day1/Console.java
+++ Testometer_Sep25/src/main/java/day1/Console.java
@@ -5,9 +5,15 @@ import java.util.Scanner;
 public class Console {
     public static void main(String[] args) {
         Scanner console=new Scanner(System.in);
-        System.out.println("what is your age");
-        int age= console.nextInt();
-        System.out.println("your age is "  +age);
+       // System.out.println("what is your age");
+       // int age= console.nextInt();
+        //System.out.println("your age is "  +age);
+        System.out.println("provide your number");
+        int num=console.nextInt();
+
+        int rem=num%10;
+        int div=num/10;
+        System.out.println(rem+div);
 
     }
 }
