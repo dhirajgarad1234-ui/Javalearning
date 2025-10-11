1574b36ad8413c1155b2f2867abeb22e99e022eb
dhirajgarad1234-ui <dhirajgarad1234@gmail.com>
2025-10-11 17:46:22 +0530

    Update ArithmaticOperator.java

--- Testometer_Sep25/src/main/java/day1/ArithmaticOperator.java
+++ Testometer_Sep25/src/main/java/day1/ArithmaticOperator.java
@@ -3,9 +3,17 @@ package day1;
 public class ArithmaticOperator {
     public static void main(String[] args) {
 
-        int num=67;
-        int sum=0;
-        sum=sum+(num%10);
-        sum=sum+num;
-        System.out.println(sum);    }
+        int i = 10;
+        int j = 7;
+        System.out.println(i + j);
+        System.out.println(i - j);
+        System.out.println(i * j);
+        System.out.println(i / j);
+        System.out.println(i % j);
+        //  int num=67;
+        //int sum=0;
+        //sum=sum+(num%10);
+        //sum=sum+num;
+        //System.out.println(sum);
+    }
 }
