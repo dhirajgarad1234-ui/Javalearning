eea010a6048d172ef975f4e3d19ae734ae0c175d
dhirajgarad1234-ui <dhirajgarad1234@gmail.com>
2025-10-18 11:15:34 +0530

    commit

---
+++ Testometer_Sep25/src/main/java/day1/Switchdemo.java
@@ -0,0 +1,19 @@
+package day1;
+
+public class Switchdemo {
+    public static void main(String[] args) {
+        int daynumber=10;
+        switch (daynumber){
+
+            case  1:
+            System.out.println("monday");
+            break;
+            case  2:
+            System.out.println("Tuesday");
+            break;
+
+            default:
+                System.out.println("No Day Found");
+        }
+    }
+}
