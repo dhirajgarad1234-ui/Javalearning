cabe60e25bf51ead0bf99941120a20c9b95b67b1
dhirajgarad1234-ui <dhirajgarad1234@gmail.com>
2025-10-18 20:31:19 +0530

    Update UrnaryOperatorDemo.java

--- Testometer_Sep25/src/main/java/day1/UrnaryOperatorDemo.java
+++ Testometer_Sep25/src/main/java/day1/UrnaryOperatorDemo.java
@@ -3,7 +3,14 @@ package day1;
 public class UrnaryOperatorDemo {
     public static void main(String[] args) {
         int a=10;
-        ++a;
-        System.out.println(a);
+        //++a;
+        //System.out.println(a);
+        //increment and decrement operator
+        //pre increment ++a first increase and assign
+        //post increment a++ first assign and increase
+        int b=++a + a++;
+        //    11+11
+        System.out.println("a :" +a);
+        System.out.println("b :" +b);
     }
 }
