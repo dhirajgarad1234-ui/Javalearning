e4db9d27d0ea02ac7bc28fe799e1e445468a3063
dhirajgarad1234-ui <dhirajgarad1234@gmail.com>
2025-10-18 19:19:37 +0530

    Update reversenumber.java

--- Testometer_Sep25/src/main/java/day1/reversenumber.java
+++ Testometer_Sep25/src/main/java/day1/reversenumber.java
@@ -2,8 +2,37 @@ package day1;
 
 public class reversenumber {
     public static void main(String[] args) {
-       //int num=34;
-       //int lastdigit=num%10;
-        //int
+      // int num=34;
+       //int lastdigit=num % 10;
+
+       //int reversenumber =0;
+      // reversenumber = reversenumber * 10 + lastdigit;//4
+      // num=num / 10;//3
+      // lastdigit=num % 10;//3
+        //reversenumber = reversenumber * 10 + lastdigit;//43
+       // System.out.println(reversenumber);
+
+        int num=3456;
+        int lastdigit=num % 10;//6
+
+        int reversenumber=0;
+        reversenumber= reversenumber * 10 + lastdigit;//6
+        num=num / 10;//345
+
+        lastdigit =num % 10;//5
+        reversenumber= reversenumber * 10 + lastdigit;//65
+        num=num / 10;//34
+
+        lastdigit=num% 10;//4
+        reversenumber= reversenumber * 10 + lastdigit;//654
+        num=num /10;//3
+
+        lastdigit=num% 10;//3
+        reversenumber= reversenumber * 10 + lastdigit;//6543
+
+        System.out.println(reversenumber);
+
+
+
     }
 }
