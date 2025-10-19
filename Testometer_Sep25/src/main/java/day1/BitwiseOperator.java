51b87088fb885a945e3a2e565c6cf8196e5db173
dhirajgarad1234-ui <dhirajgarad1234@gmail.com>
2025-10-19 11:10:29 +0530

    Create BitwiseOperator.java

---
+++ Testometer_Sep25/src/main/java/day1/BitwiseOperator.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class BitwiseOperator {
+    public static void main(String[] args) {
+        //ANT OR operator
+        // AND-- True & True=True
+        //       True & False=False
+        //       False & True=False
+        //       False & False=False
+        //OR-- True Or True= True
+        //     True Or False=True
+        //     False or True=True
+        //     False or False=False
+    }
+}
